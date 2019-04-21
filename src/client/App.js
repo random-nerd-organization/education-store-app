ecf3e16c6c7f8a85694ad8bea9046df392823150
jew-er <emil@pernik.org>
2019-04-22 00:09:37 +0300

    added navigation menu

--- src/client/App.js
+++ src/client/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import React, { Component } from "react";
+import "./app.css";
 
 export default class App extends Component {
   state = { username: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
   }
 
   render() {
-    const { username } = this.state;
     return (
-      <div>
-        {username ? <h1>{`Hello :))) ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-        <h1>bonjour elvis </h1>
-      </div>
+      <body>
+        <nav>
+          <div className="nav-item">Store</div>
+          <div className="nav-item">Sales</div>
+          <div className="nav-item"> About Us</div>
+          <div className="nav-item">Your profile</div>
+        </nav>
+      </body>
     );
   }
 }
