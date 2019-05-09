fdddda4c7e685c2ce17cafb264b2508a9a934106
jewer <emil@pernik.org>
2019-05-09 20:04:15 +0300

    test commit

--- src/client/Pages/Home.js
+++ src/client/Pages/Home.js
@@ -1,32 +1,24 @@
 import React, { Component } from "react";
 import "./Home.css";
 import Form from "../Components/Form";
+
+
 export default class Home extends Component {
-
-    render() {
-        return (
-            <main>
-                <section>
-                    <div className="welcome">
-
-                        <div className="text">
-                            ReSize Design
-                        </div>
-                        <div className="link-container">
-                            <div className="link">
-                                Women Clothes
-                            </div>
-                            <div className="link">
-                                Men Clothes
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <Form/>
-                <footer>
-
-                </footer>
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main>
+        <section>
+          <div className="welcome">
+            <div className="text">ReSize Design</div>
+            <div className="link-container">
+              <div className="link">Women Clothes</div>
+              <div className="link">Men Clothes</div>
+            </div>
+          </div>
+        </section>
+        <Form />
+        <footer />
+      </main>
+    );
+  }
 }
